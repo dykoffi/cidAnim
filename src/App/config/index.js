9a531d592a5f2870a5317dee0de0ceff69484631
dy koffi <dykoffi2000@gmail.com>
2021-10-02 12:06:47 +0000

    compact routes

--- src/App/config/index.js
+++ src/App/config/index.js
@@ -1,11 +1,16 @@
 import React from "react"
-import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "./routes";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "../pages/home"
+import NotFound from "../pages/NotFound"
 
 function App() {
   return (
     <Router>
-      <Routes />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
